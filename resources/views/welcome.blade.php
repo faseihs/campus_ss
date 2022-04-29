b531545d7a80d26d9856784365ffb79bb9ba8964
Faseih Saad <faseihsaad@gmail.com>
2022-04-30 02:21:38 +0500

    upgrade to laravel 8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Campus Security System
+                    Campus System
                 </div>
             </div>
         </div>
